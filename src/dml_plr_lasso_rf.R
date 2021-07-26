d452951533472771fb4f4fd7b2b3eee8d064453c
SignorinoY <meetziyang@gmail.com>
2021-07-26 10:03:34 +0800

    :zap: update dml_plr_lasso_rf.R

--- src/dml_plr_lasso_rf.R
+++ src/dml_plr_lasso_rf.R
@@ -6,12 +6,12 @@ dml_plr_lasso_rf <- function(df) {
     theta_ols <- model_lm$coefficients["d"]
 
     # Double Machine Learning with LASSO
-    learner <- lrn("regr.cv_glmnet", nfolds = 10, s = "lambda.min")
+    learner <- lrn("regr.cv_glmnet", nfolds = 3, s = "lambda.min")
     ml_g <- learner$clone()
     ml_m <- learner$clone()
     dml_lasso <- DoubleMLPLR$new(
         df_dml, ml_g, ml_m,
-        n_folds = 10
+        n_folds = 5
     )
     dml_lasso$fit()
     theta_dml_lasso <- dml_lasso$coef
@@ -22,20 +22,18 @@ dml_plr_lasso_rf <- function(df) {
     ml_m <- learner$clone()
     param_grid <- list(
         "ml_g" = ParamSet$new(list(
-            ParamInt$new("mtry", lower = 2, upper = 16),
-            ParamInt$new("num.trees", lower = 16, upper = 256),
-            ParamInt$new("max.depth", lower = 2, upper = 16)
+            ParamInt$new("num.trees", lower = 128, upper = 1024),
+            ParamInt$new("max.depth", lower = 4, upper = 256)
         )),
         "ml_m" = ParamSet$new(list(
-            ParamInt$new("mtry", lower = 2, upper = 16),
-            ParamInt$new("num.trees", lower = 16, upper = 256),
-            ParamInt$new("max.depth", lower = 2, upper = 16)
+            ParamInt$new("num.trees", lower = 128, upper = 1024),
+            ParamInt$new("max.depth", lower = 4, upper = 256)
         ))
     )
     tune_settings <- list(
-        terminator = mlr3tuning::trm("evals", n_evals = 100),
+        terminator = mlr3tuning::trm("evals", n_evals = 20),
         algorithm = tnr("random_search"),
-        rsmp_tune = rsmp("cv", folds = 5),
+        rsmp_tune = rsmp("cv", folds = 3),
         measure = list(
             "ml_g" = msr("regr.mse"),
             "ml_m" = msr("regr.mse")
@@ -44,12 +42,12 @@ dml_plr_lasso_rf <- function(df) {
 
     dml_rf <- DoubleMLPLR$new(
         df_dml, ml_g, ml_m,
-        n_folds = 10
+        n_folds = 5
     )
     dml_rf$tune(
         param_set = param_grid,
         tune_settings = tune_settings,
-        tune_on_folds = FALSE
+        tune_on_folds = TRUE
     )
     dml_rf$fit()
     theta_dml_rf <- dml_rf$coef
