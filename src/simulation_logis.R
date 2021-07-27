4e99cd1befa6caa32d088dcf58de48bf2c486ab1
SignorinoY <meetziyang@gmail.com>
2021-07-27 14:32:00 +0800

    :zap: update simulation_logis.R

--- src/simulation_logis.R
+++ src/simulation_logis.R
@@ -14,47 +14,46 @@ source("./src/dml_plr_lasso_rf.R")
 cores <- 100
 iters <- 1000
 
-# Data Generation
-
-n <- 1000
-p <- 3
-mu <- rep(0, p)
-sigma <- diag(p)
+n <- 200
 
 theta <- 1
-
-data <- list()
-for (iter in 1:iters) {
-    set.seed(iter)
-    x <- mvrnorm(n, mu = mu, Sigma = sigma)
-    d <- plogis(x[, 1] + x[, 2]) + x[, 3] + rnorm(n)
-    y <- theta * d + x[, 1] + x[, 2] + plogis(x[, 3]) + rnorm(n)
-    data[[iter]] <- data.frame(X = x, d = d, y = y)
-}
-
-# Parallelization Estimation
 
 cl <- makeCluster(cores)
 registerDoSNOW(cl)
 pb <- txtProgressBar(max = iters, style = 3)
 progress <- function(n) setTxtProgressBar(pb, n)
-opts <- list(progress = progress)
+for (p in c(3, 5, 10, 20, 50, 100, 150)) {
+    mu <- rep(0, p)
+    sigma <- diag(p)
 
-simulation_result <- foreach(
-    iter = 1:iters,
-    .combine = rbind,
-    .options.snow = opts,
-    .packages = c("mlr3", "mlr3learners", "DoubleML", "mlr3tuning")
-) %dopar% {
-    df <- data[[iter]]
-    theta_hat <- dml_plr_lasso_rf(df)
+    # Data Generation
+    data <- list()
+    for (iter in 1:iters) {
+        set.seed(iter)
+        x <- mvrnorm(n, mu = mu, Sigma = sigma)
+        d <- plogis(x[, 1] + x[, 2]) + x[, 3] + rnorm(n)
+        y <- theta * d + x[, 1] + x[, 2] + plogis(x[, 3]) + rnorm(n)
+        data[[iter]] <- data.
+        frame(X = x, d = d, y = y)
+    }
+    # Parallelization Estimation
+    opts <- list(progress = progress)
+    simulation_result <- foreach(
+        iter = 1:iters,
+        .combine = rbind,
+        .options.snow = opts,
+        .packages = c("mlr3", "mlr3learners", "DoubleML", "mlr3tuning")
+    ) %dopar% {
+        df <- data[[iter]]
+        theta_hat <- dml_plr_lasso_rf(df)
+    }
+
+    colnames(simulation_result) <- c("OLS", "DML.LASSO", "DML.RF")
+    write.csv(
+        simulation_result,
+        file = paste("./data/simulation_logis_", p, ".csv", sep = ""),
+        row.names = FALSE
+    )
 }
 
 stopCluster(cl)
-
-colnames(simulation_result) <- c("OLS", "DML.LASSO", "DML.RF")
-write.csv(
-    simulation_result,
-    file = "./data/simulation_logis.csv",
-    row.names = FALSE
-)
